baeb52e3bdc97462440764d3afb239343855cae5
atrox3d <atrox3d@gmail.com>
2024-08-07 17:13:40 +0200

    playing with sqlite

---
+++ models/post.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+    created_at: str
+    id: int = None
+
+class Posts(BaseModel):
+    posts: list[Post] = []
+    # count: int = 0
+
+    def add(self, post:Post):
+        self.posts.append(post)
+        # self.count = len(self.posts)
