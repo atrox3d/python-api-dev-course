140ffb7d748b235def013701b8d2ba86123cd7d5
atrox3d <atrox3d@gmail.com>
2024-08-13 18:14:00 +0200

    add boolean and timestamp with server default value

--- orm/models.py
+++ orm/models.py
@@ -1,4 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+# from sqlalchemy.sql.expression import text
+from sqlalchemy.sql import func
 import sqlalchemy as sa
 from .sqlite import Base
 
@@ -8,4 +11,10 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, 
+                    #    default=True,
+                       server_default='true'
+                       )
+    created_at = Column(TIMESTAMP(timezone=True),   # timestamp data type
+                        nullable=False,
+                        server_default=func.now())  # server side timestamp
