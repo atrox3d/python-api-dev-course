49a2b34f6ce6f2b47a8d1eaf5783c0a27b0925e9
atrox3d <atrox3d@gmail.com>
2024-08-18 17:25:27 +0200

    print access token components

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 
 # SECRET_KEY
 # alghorytm
@@ -13,8 +13,28 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data:dict):
+    # work on data shallow copy
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # get current time
+    now = datetime.now(
+        # UTC\
+    )
+    # time difference
+    delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # expire time
+    expire = now + delta
+    # add expire time to data
     to_encode['exp'] = expire
+    # encode everything
     token = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+
+    print(f'TOKEN| {ACCESS_TOKEN_EXPIRE_MINUTES = }')
+    print(f'TOKEN| {SECRET_KEY = }')
+    print(f'TOKEN| {ALGORITHM  = }')
+    print(f'TOKEN| {now        = }')
+    print(f'TOKEN| {expire     = }')
+    print(f'TOKEN| {to_encode  = }')
+    print(f'TOKEN| {token      = }')
+
     return token
+
--- schemas/user.py
+++ schemas/user.py
@@ -15,3 +15,10 @@ Users = list[UserCreate]
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: str | None = None
