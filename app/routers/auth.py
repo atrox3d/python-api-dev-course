5200dc580ee8c660f011b822a00454c83e469b66
atrox3d <atrox3d@gmail.com>
2024-08-18 06:27:41 +0200

    add OAuth2PasswordRequestForm to login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+
 from sqlalchemy.orm import Session
 
 from db.orm.sqlite import get_db
@@ -10,9 +12,13 @@ import app.oauth2
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(credentials:schemas.user.UserLogin, db: Session = Depends(get_db)):
+def login(
+            # credentials:schemas.user.UserLogin, 
+            credentials:OAuth2PasswordRequestForm = Depends(), 
+            db: Session = Depends(get_db)
+):
     user: models.User = db.query(models.User).filter(
-        models.User.email==credentials.email).first()
+        models.User.email==credentials.username).first()
     
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
