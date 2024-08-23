6febb721466cccd04f74b2f04533d7f3e720d341
atrox3d <atrox3d@gmail.com>
2024-08-23 10:11:55 +0200

    add import from file/all

--- app/routers/utilities/db.py
+++ app/routers/utilities/db.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 
 from db.orm.sqlite import get_db
 from db.orm import models
+import helpers.db
 
 router = APIRouter(prefix='/db', tags=['util/db'])
 
@@ -13,19 +14,13 @@ def db():
     return {'db': 'welcome to the db utility'}
 
 
-def get_tables_models(module) -> dict:
-    return {model.__tablename__:model for model in
-         [getattr(module, name) for name in dir(module)]
-         if getattr(model, '__tablename__', None) is not None
-         }
-
 @router.get('/table/show/{tablename}')
 def db(
         tablename:str,
         db: Session = Depends(get_db),
 ):
     try:
-        model = get_tables_models(models)[tablename]
+        model = helpers.db.get_tables_models(models)[tablename]
         rows = db.query(model).all()
         return {tablename: rows}
     except KeyError:
@@ -43,16 +38,13 @@ def db(
 ):
     print(tablename, filename, delete)
     try:
-        model = get_tables_models(models)[tablename]
-
-        if delete:
-            db.query(model).delete()
-        
-        with open(filename) as fp:
-            data = json.load(fp)
-        for row in data:
-            db.add(model(**row))
-        db.commit()
+        helpers.db.import_table_from_json(
+            db,
+            models,
+            tablename,
+            filename,
+            delete_existing=delete
+        )
     except KeyError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -71,3 +63,32 @@ def db(
 
 
 
+@router.get('/table/import/all')
+def db(
+        delete:bool=False,
+        db: Session = Depends(get_db),
+):
+    try:
+        helpers.db.import_all(
+            db,
+            models,
+            'users',
+            'posts',
+            delete_existing=delete
+        )
+
+    except KeyError as ke:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=repr(ke)
+        )
+    except FileNotFoundError as fnfe:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=repr(fnfe)
+        )
+    except json.decoder.JSONDecodeError as jde:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=repr(jde)
+        )
--- helpers/db.py
+++ helpers/db.py
@@ -1,5 +1,6 @@
 import json
 from types import ModuleType
+from typing import Callable
 from fastapi import HTTPException, status, Depends, APIRouter
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
@@ -10,7 +11,57 @@ import schemas.post
 import schemas.user
 import app.utils
 
+def get_tables_models(module) -> dict:
+    return {
+        model.__tablename__:model for model in
+        [getattr(module, name) for name in dir(module)]
+        if getattr(model, '__tablename__', None) is not None
+    }
 
+def import_table_from_json(
+        db:Session,
+        models:ModuleType,
+        tablename:str,
+        filename:str=None,
+        json_provider:Callable=None,
+        delete_existing:bool=False
+):
+    model = get_tables_models(models)[tablename]
+
+    if delete_existing:
+        db.query(model).delete()
+
+    if filename is not None:
+        with open(filename) as fp:
+            data = json.load(fp)
+        for row in data:
+            db.add(model(**row))
+        db.commit()
+    elif json_provider is not None:
+        pass
+    else:
+        raise ValueError(
+            'at least one of filename, json_provider must have a value'
+        )
+
+def import_all(
+        db:Session,
+        models:ModuleType,
+        *tablenames:str,
+        # tablename:str,
+        # filename:str=None,
+        json_provider:Callable=None,
+        delete_existing:bool=False
+):
+    for tablename in tablenames:
+        filename = f'{tablename}.json'
+        import_table_from_json(
+            db,
+            models,
+            tablename,
+            filename,
+            delete_existing=delete_existing
+        )
 
 
 
@@ -75,4 +126,5 @@ def _reset_db(
 
 
     _db.close()
+
 
