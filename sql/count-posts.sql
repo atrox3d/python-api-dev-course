330a85731cfa41bc8b8a5324fd95ecc37deb486b
atrox3d <atrox3d@gmail.com>
2024-08-24 05:22:20 +0200

    add some sql examples

---
+++ sql/count-posts.sql
@@ -0,0 +1,22 @@
+-- SQLite
+
+-- show users and posts ordered by user
+SELECT users.id, users.email, posts.id, posts.title
+FROM posts
+RIGHT JOIN users
+on users.id = posts.owner_id
+ORDER BY users.id;
+
+-- show count of posts per user, including users without posts (NULL)
+SELECT users.id, COUNT(*) 
+FROM posts
+RIGHT JOIN users
+ON posts.owner_id = users.id
+GROUP BY users.id;
+
+-- show count of posts per user, excluding users without posts (NULL)
+SELECT users.id, COUNT(posts.id)
+FROM posts
+RIGHT JOIN users
+on users.id = posts.owner_id
+GROUP BY users.id;
