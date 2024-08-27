0fdb7b4fe64aa5aea2c6234ea3bbd034a2e85a38
atrox3d <atrox3d@gmail.com>
2024-08-27 16:12:10 +0200

    add user data fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,9 @@
+import pytest
+
 import schemas.user
 import tests.database
 from tests.database import client, session
+
 
 
 DEBUG = False
@@ -10,6 +13,24 @@ def debug(*args, force=False, **kwargs):
 
 tests.database.debug = debug
 
+@pytest.fixture
+def user_create() -> schemas.user.UserCreate:
+    return schemas.user.UserCreate(
+        email='testuser@gmail.com',
+        password='password'
+    )
+
+@pytest.fixture
+def user_create_json(user_create) -> dict[str, str]:
+    return user_create.model_dump()
+
+@pytest.fixture
+def user_login_json(user_create) -> dict[str, str]:
+    return {
+        'username': user_create.email, 
+        'password': user_create.password
+    }
+
 # session fixture is cached
 def test_root(client, session):
     debug(f'\nTEST_ROOT| {session.rand = }', force=False)
@@ -18,32 +39,27 @@ def test_root(client, session):
     assert response.json().get('message') == 'welcome to my api'
 
 # session fixture is cached
-def test_create_user(client, session):
+def test_create_user(client, session, user_create_json):
     debug(f'\nTEST_CREATE_USER| {session.rand = }', force=False)
     debug()
     debug(f'TEST_CREATE_USER| get_user')
     response = client.post(
                         '/users',
-                        json={
-                            'email': 'testuser@gmail.com',
-                            'password': 'password'
-                        }
+                        json=user_create_json
     )
     assert response.status_code == 201
     user = schemas.user.UserOut(**response.json())
     assert user.email == 'testuser@gmail.com'
 
 # session fixture is cached
-def test_login_user(client, session):
+def test_login_user(client, session, user_login_json):
     debug(f'\nTEST_LOGIN_USER| {session.rand = }', force=False)
     debug()
     debug(f'TEST_LOGIN_USER| get_user')
+    print(user_login_json)
     response = client.post(
                         '/login',
-                        data={
-                            'username': 'testuser@gmail.com',
-                            'password': 'password'
-                        }
+                        data=user_login_json
     )
     print(response)
     print(response.json())
