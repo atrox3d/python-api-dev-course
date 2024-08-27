a189060d1a9741dd24125780c9186d3e90f2ccea
atrox3d <atrox3d@gmail.com>
2024-08-27 17:37:51 +0200

    add docstrings to fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -16,6 +16,7 @@ tests.database.debug = debug
 
 @pytest.fixture
 def user_create() -> schemas.user.UserCreate:
+    ''' returns a UserCreate instance for validation/input'''
     return schemas.user.UserCreate(
         email='testuser@gmail.com',
         password='password'
@@ -23,10 +24,12 @@ def user_create() -> schemas.user.UserCreate:
 
 @pytest.fixture
 def user_create_json(user_create) -> dict[str, str]:
+    ''' returns json of user_create '''
     return user_create.model_dump()
 
 @pytest.fixture
 def user_login_json(user_create) -> dict[str, str]:
+    ''' returns converted user_create for login '''
     return {
         'username': user_create.email, 
         'password': user_create.password
@@ -34,6 +37,7 @@ def user_login_json(user_create) -> dict[str, str]:
 
 @pytest.fixture
 def new_user(client, user_create, user_create_json) -> schemas.user.UserDb:
+    ''' creates new user and return model of it from db '''
     response = client.post(
                         '/users',
                         json=user_create_json
