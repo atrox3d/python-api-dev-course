2fd90daa43b5b0d5adca0ccb32fff268882a930f
atrox3d <atrox3d@gmail.com>
2024-08-27 08:19:53 +0200

    test_create_user

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,3 +7,15 @@ def test_root():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json().get('message') == 'welcome to my api'
+
+def test_create_user():
+    response = client.post(
+                '/users',
+                json={
+                    'email': 'testuser@gmail.com',
+                    'password': 'password'
+                }
+    )
+    print(response.json())
+    assert response.status_code == 201
+
