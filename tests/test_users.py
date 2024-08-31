05e59cb16145d22dbb92bfd153df41268a9e2105
atrox3d <atrox3d@gmail.com>
2024-08-31 07:37:05 +0200

    adding annotations, cleaning up

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,51 +1,52 @@
-import pytest
+import logging
+from fastapi.testclient import TestClient
 
 import schemas.user
-# import tests.database
-# from tests.database import client, session
 import app.oauth2
-from tests.debug import debug
-import tests.debug
 
-
-def  setup_module():
-    tests.debug.DEBUG = True
+logger = logging.getLogger(__name__)
 
 # session fixture is cached
-def test_root(unauthorized_client, session):
-    debug(f'\nTEST_ROOT| {session.rand = }', force=False)
+def test_root(
+        unauthorized_client: TestClient, 
+        session: app.oauth2.Session
+):
+    logger.debug(f'{session.rand = }')
     response = unauthorized_client.get('/')
     assert response.status_code == 200
     assert response.json().get('message') == 'welcome to my api'
 
 # session fixture is cached
-def test_create_user(unauthorized_client, session, user_creation_json):
-    debug(f'\nTEST_CREATE_USER| {session.rand = }', force=False)
-    debug()
-    debug(f'TEST_CREATE_USER| get_user')
+def test_create_user(
+        unauthorized_client: TestClient, 
+        session: app.oauth2.Session, 
+        user_creation_json: dict[str, str]
+):
+    logger.debug(f'{session.rand = }')
+    logger.debug(f'get_user')
     response = unauthorized_client.post(
                         '/users',
                         json=user_creation_json
     )
     assert response.status_code == 201
+
     user = schemas.user.UserOut(**response.json())
     assert user.email == 'testuser@gmail.com'
 
 # session fixture is cached
-def test_login(unauthorized_client, session, user_login_json, add_user_db_id1):
-    debug(f'\nTEST_LOGIN_USER| {session.rand = }', force=False)
-    debug()
-    debug(f'TEST_LOGIN_USER| get_user')
-    print(user_login_json)
+def test_login(
+        unauthorized_client: TestClient, 
+        session: app.oauth2.Session, 
+        user_login_json: dict[str, str], 
+        add_user_db_id1: schemas.user.UserDb
+):
+    logger.debug(f'{session.rand = }')
+    logger.debug(f'get_user')
     response = unauthorized_client.post(
                         '/login',
                         data=user_login_json
     )
-    print(response)
-    print(response.json())
-    token = schemas.user.Token(
-        **response.json()
-    )
+    token = schemas.user.Token(**response.json())
     tokendata = app.oauth2.verify_access_token(
         token.access_token,
         app.oauth2.unhautorized_exception
